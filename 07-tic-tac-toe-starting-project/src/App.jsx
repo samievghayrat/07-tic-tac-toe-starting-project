aa72afcfc80a6a6eac10b9da2f0df6340a6785c1
ghayratsamiev <Ghayrat Samiev>
2024-03-18 15:28:27 +0900

    Added player names on the screen

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player.jsx";
 
 function App() {
-  
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+
+    return <main>
+        <div id="game-container">
+            <ol id="players">
+                <Player name="Player one" symbol="X"></Player>
+                <Player name="Player 2" symbol="X"></Player>
+
+            </ol>
+            <h5>GAME BOARD</h5>
+        </div>
+    </main>
 }
 
 export default App
